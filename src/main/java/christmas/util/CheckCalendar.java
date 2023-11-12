e699aa599ea678a1870ba3f9e84e0b386c28045f
Lee Geun Woo <gamebdy@naver.com>
2023-11-12 21:59:27 +0900

    :sparkles: CheckCalendar 기능 구현

--- src/main/java/christmas/util/CheckCalendar.java
+++ src/main/java/christmas/util/CheckCalendar.java
@@ -5,11 +5,13 @@ import java.util.Calendar;
 public class CheckCalendar {
 
     private final Calendar calendar;
+    private static final int DEFAULT_YEAR = 2023;
+    private static final int DEFAULT_MONTH = 12;
 
     public CheckCalendar(){
         this.calendar = Calendar.getInstance();
-        this.calendar.set(Calendar.YEAR, 2023);
-        this.calendar.set(Calendar.MONTH, 12);
+        this.calendar.set(Calendar.YEAR, DEFAULT_YEAR);
+        this.calendar.set(Calendar.MONTH, DEFAULT_MONTH);
     }
 
     public int getDayOfWeek(int inputDate) {
@@ -17,19 +19,19 @@ public class CheckCalendar {
         return calendar.get(Calendar.DAY_OF_WEEK);
     }
 
-    public static boolean isWeekday(int dayOfWeek) {
+    public boolean isWeekday(int dayOfWeek) {
         return dayOfWeek >= Calendar.MONDAY && dayOfWeek <= Calendar.FRIDAY;
     }
 
-    public static boolean isWeekend(int dayOfWeek) {
+    public boolean isWeekend(int dayOfWeek) {
         return dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY;
     }
 
-    public static boolean isSpecial(int dayOfMonth, int dayOfWeek) {
+    public boolean isSpecial(int dayOfMonth, int dayOfWeek) {
         return dayOfMonth == 25 || dayOfWeek == Calendar.SUNDAY;
     }
 
-    public static boolean isAfterChristmas(int dayOfMonth) {
+    public boolean isAfterChristmas(int dayOfMonth) {
         return dayOfMonth > 25 ;
     }
 }
