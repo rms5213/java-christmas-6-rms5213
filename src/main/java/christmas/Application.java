7c3d04c2c7a6c9bebdd538a6c67ae86788a4b38a
Lee Geun Woo <gamebdy@naver.com>
2023-11-12 21:58:58 +0900

    :sparkles: Controller 에 인자 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,20 +2,22 @@ package christmas;
 
 import christmas.View.InputView;
 import christmas.View.OutputView;
+import christmas.View.MenuValidation;
 import christmas.util.CheckCalendar;
-
+import christmas.Model.Menu;
 import christmas.Controller.Controller;
 
-import javax.swing.*;
 
 public class Application {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
+        MenuValidation menuValidation = new MenuValidation();
+        InputView inputView = new InputView(menuValidation);
         OutputView outputView = new OutputView();
         CheckCalendar checkCalendar = new CheckCalendar();
+        Menu menu = new Menu();
 
-        Controller controller = new Controller(inputView, outputView, checkCalendar);
+        Controller controller = new Controller(inputView, outputView, checkCalendar, menu);
         controller.run();
     }
 }
