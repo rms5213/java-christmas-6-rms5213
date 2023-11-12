5784baf485565b77216d9322f5c1efa8c346d4ff
Lee Geun Woo <gamebdy@naver.com>
2023-11-12 23:29:39 +0900

    :sparkles: 기능 1차 구현에 따른 OutputView 수정

--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -22,17 +22,41 @@ public class OutputView {
         System.out.println(String.format("%,d", totalPrice) + "원");
     }
 
-    public void printBenefitMenu() {
+    public void printBenefitMenu(boolean present) {
         System.out.println("<증정 메뉴>");
+        if(!present) doesntExist();
+        if(present) System.out.println("샴페인 1개");
     }
 
-    public void printBenefitList() {
+    public void printBenefitList(int dDay, int week, int special, int total) {
+        int result = dDay + week + special;
         System.out.println("<혜택 내역>");
+        if(result == 0 && total < 120000) doesntExist();
+        if(result != 0){
+            if(dDay != 0) {
+                System.out.print("크리스마스 디데이 할인: ");
+                System.out.println(String.format("%,d", (-1) * dDay) + "원");
+            }
+            if(week != 0) {
+                System.out.print("평일 할인: ");
+                System.out.println(String.format("%,d", (-1) * week) + "원");
+            }
+            if(special != 0) {
+                System.out.print("특별 할인: ");
+                System.out.println(String.format("%,d", (-1) * special) + "원");
+            }
+            if(total >= 120000) {
+                System.out.print("증정 이벤트: ");
+                System.out.println(String.format("%,d", -25000) + "원");
+            }
+        }
+
+
     }
 
     public void printTotalBenefitPrice(int benefitPrice) {
         System.out.println("<총혜택 금액>");
-        System.out.println(String.format("%,d", benefitPrice) + "원");
+        System.out.println(String.format("%,d", (-1) * benefitPrice) + "원");
     }
 
     public void printPriceAfterDiscount(int priceAfterDisCount) {
@@ -40,8 +64,19 @@ public class OutputView {
         System.out.println(String.format("%,d", priceAfterDisCount) + "원");
     }
 
-    public void printBadge() {
+    public void printBadge(int totalBenefitPrice) {
         System.out.println("<12월 이벤트 배지>");
+        if(totalBenefitPrice < 5000) doesntExist();
+        if(5000 <= totalBenefitPrice && totalBenefitPrice < 10000)
+            System.out.println("별");
+        if(10000 <= totalBenefitPrice && totalBenefitPrice < 20000)
+            System.out.println("트리");
+        if(20000 <= totalBenefitPrice)
+            System.out.println("산타");
+    }
+
+    public void doesntExist(){
+        System.out.println("없음");
     }
 
 }
