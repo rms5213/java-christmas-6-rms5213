6cc42a2ee50f1bb9ede632a9093b937837d862de
Lee Geun Woo <gamebdy@naver.com>
2023-11-14 12:22:19 +0900

    :sparkles: [ERROR] 문구 위치 수정

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -28,7 +28,7 @@ public class InputView {
 
         // 날짜 유효성 검증
         while (!dateValidation.isValidDate(input)) {
-            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            //System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
             input = Console.readLine();
         }
 
@@ -38,7 +38,7 @@ public class InputView {
 
     public Map<String, Integer> readMenu() {
         System.out.print("주문하실 메뉴를 메뉴와 개수를 알려 주세요. ");
-        System.out.println("e.g. 해산물파스타-2,레드와인-1,타파스-1,제로콜라-1");
+        System.out.println("(e.g. 해산물파스타-2,레드와인-1,타파스-1,제로콜라-1)");
         String input = Console.readLine();
         while(true){
             Map <String, Integer > menuMap = menuValidation.validateMenuInput(input);
