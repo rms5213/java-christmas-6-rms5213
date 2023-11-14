b439757646a0462f15ec4cb547436c20ad553003
Lee Geun Woo <gamebdy@naver.com>
2023-11-14 12:22:34 +0900

    :sparkles: Validation 기능 추가

--- src/main/java/christmas/View/MenuValidation.java
+++ src/main/java/christmas/View/MenuValidation.java
@@ -7,12 +7,13 @@ import java.util.regex.Pattern;
 
 public class MenuValidation {
 
-    private static final String MENU_PATTERN = "([가-힣a-zA-Z0-9]+)-([1-9][0-9]*)";
+    private static final String MENU_PATTERN = "([가-힣]+)-([1-9][0-9]*)";
     private static final Pattern pattern = Pattern.compile(MENU_PATTERN);
 
     public Map<String, Integer> validateMenuInput(String input) {
         Map<String, Integer> menuMap = new HashMap<>();
         String[] orders = input.split(",");
+        int totalOrder = 0;
         for (String order : orders) {
             if (isValidFormat(order)) {
                 String[] menuAndCount = order.split("-");
@@ -22,6 +23,12 @@ public class MenuValidation {
                 // 메뉴 이름과 주문 개수의 형식 검사
                 if (isValidMenu(menu) && isValidCount(count)) {
                     menuMap.put(menu, count);
+                    totalOrder += count;
+                    if(isExceedMax(totalOrder)){
+                        printErrorMessage("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.");
+                        menuMap = null;
+                        break;
+                    }
                 } else {
                     printErrorMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
                 }
@@ -49,5 +56,9 @@ public class MenuValidation {
 
     private void printErrorMessage(String message) {
         System.out.println(message);
+    }
+
+    private boolean isExceedMax(int totalCount) {
+        return totalCount > 20;
     }
 }
