5a0dcbd6951d05c207cc57ee55ddd7680b7c4947
Lee Geun Woo <gamebdy@naver.com>
2023-11-15 12:25:31 +0900

    :sparkles: DateValidation 의 출력문 상수화

--- src/main/java/christmas/View/DateValidation.java
+++ src/main/java/christmas/View/DateValidation.java
@@ -3,12 +3,16 @@ package christmas.View;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class DateValidation {
+    private static final String ERROR_INVALID_DATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final int MONTH_START = 1;
+    private static final int MONTH_END = 31;
+
     private static final String DATE_PATTERN = "([1-9]|[12][0-9]|3[01])";
     private static final Pattern PATTERN = Pattern.compile(DATE_PATTERN);
 
     public boolean isValidDate(String input) {
         boolean isValid = isValidFormat(input) && isValidRange(input);
-        if(!isValid) System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        if(!isValid) System.out.println(ERROR_INVALID_DATE);
         return isValid;
     }
 
@@ -19,6 +23,6 @@ public class DateValidation {
 
     private boolean isValidRange(String input) {
         int date = Integer.parseInt(input);
-        return date >= 1 && date <= 31;
+        return date >= MONTH_START && date <= MONTH_END;
     }
 }
