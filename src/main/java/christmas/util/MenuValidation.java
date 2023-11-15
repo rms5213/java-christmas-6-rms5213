344b29e1fab79a2511ba59a5e4f2dd587c447749
Lee Geun Woo <gamebdy@naver.com>
2023-11-15 17:04:46 +0900

    :sparkles: Validation 순서 수정

--- src/main/java/christmas/util/MenuValidation.java
+++ src/main/java/christmas/util/MenuValidation.java
@@ -2,8 +2,6 @@ package christmas.util;
 
 import christmas.Model.Menu;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -13,6 +11,9 @@ import java.util.regex.Pattern;
 
 public class MenuValidation {
 
+    private static final String ERROR_INVALID_MENU = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+
     private static final String MENU_PATTERN = "([가-힣]+)-([1-9][0-9]*)";
     private static final Pattern pattern = Pattern.compile(MENU_PATTERN);
 
@@ -20,9 +21,6 @@ public class MenuValidation {
     private static final int MAX_ORDER = 20;
     private final Menu menu;
     boolean isOrderCorrect = false;
-
-
-    List<String> drinkMenuList = Arrays.asList("제로콜라", "레드와인", "샴페인"); //enum화 필요
 
 
     public MenuValidation(Menu menu) {
@@ -34,35 +32,38 @@ public class MenuValidation {
         Set<String> uniqueMenus = new HashSet<>(); // 중복 체크를 위한 Set
         String[] orders = input.split(",");
         int totalOrder = 0;
-        for (String order : orders) {
-            if (isValidFormat(order)) {
+
+        try {
+            boolean checkNonDrink = false;
+            for (String order : orders) {
+
+                checkException(!isValidFormat(order));
+
                 String[] menuAndCount = order.split("-");
                 String menu = menuAndCount[0];
                 int count = Integer.parseInt(menuAndCount[1]);
 
-                if (!uniqueMenus.add(menu)) {
-                    printErrorMessage("[ERROR] 중복된 메뉴가 있습니다. 다시 입력해 주세요.");
-                    return null;
+
+                checkException(!uniqueMenus.add(menu));
+
+                if (isValidOrder(menu)) {
+                    checkNonDrink = true;
                 }
 
-                // 메뉴 이름과 주문 개수의 형식 검사
-                if (isValidMenu(menu) && isValidCount(count) && isValidOrder(menu)) {
-                    menuMap.put(menu, count);
-                    totalOrder += count;
-                    if (isExceedMax(totalOrder)) {
-                        printErrorMessage("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.");
-                        return null;
-                    }
-                } else {
-                    printErrorMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-                    return null;
-                }
+                checkException(!isValidMenu(menu) || !isValidCount(count));
 
-            } else {
-                printErrorMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-                return null;
+                menuMap.put(menu, count);
+                totalOrder += count;
+
+                checkException(isExceedMax(totalOrder));
+
             }
+            checkException(!checkNonDrink);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_INVALID_MENU);
+            return null;
         }
+
         return menuMap;
     }
 
@@ -80,9 +81,6 @@ public class MenuValidation {
         return count > NONE;
     }
 
-    private void printErrorMessage(String message) {
-        System.out.println(message);
-    }
 
     private boolean isExceedMax(int totalCount) {
         return totalCount > MAX_ORDER;
@@ -95,8 +93,15 @@ public class MenuValidation {
         return isOrderCorrect;
     }
 
-    private boolean isDrink(String menu) {
-        return drinkMenuList.contains(menu);
+    private boolean isDrink(String menuName) {
+        Menu.Category category = menu.getCategory(menuName);
+        return category == Menu.Category.DRINK;
     }
 
+
+    private void checkException(boolean validation) {
+        if (validation) {
+            throw new IllegalArgumentException(ERROR_INVALID_MENU);
+        }
+    }
 }
