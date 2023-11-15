abe05caea464e41b4e39c7d82582690c729be38d
Lee Geun Woo <gamebdy@naver.com>
2023-11-15 19:36:18 +0900

    :sparkles: InputView 출력문구 수정

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -8,7 +8,7 @@ import christmas.util.MenuValidation;
 public class InputView {
     private static final String INPUT_INFORMATION_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String RESERVATION_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    private static final String ORDER_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요.Z";
+    private static final String ORDER_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요.";
     private static final String ORDER_EXAMPLE = "(e.g. 해산물파스타-2,레드와인-1,타파스-1,제로콜라-1)";
 
 
