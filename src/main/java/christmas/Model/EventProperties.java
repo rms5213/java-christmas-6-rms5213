f24cfe0e8aed329a8db09ff7eb507ca0710b0334
Lee Geun Woo <gamebdy@naver.com>
2023-11-15 14:05:43 +0900

    :sparkles: Controller 의 checkDate 를 분리하기위해 EventProperties 생성

---
+++ src/main/java/christmas/Model/EventProperties.java
@@ -0,0 +1,30 @@
+package christmas.Model;
+
+public class EventProperties {
+    private final boolean isWeekday;
+    private final boolean isWeekend;
+    private final boolean isSpecial;
+    private final boolean isAfterChristmas;
+
+    public EventProperties(boolean isWeekday, boolean isWeekend, boolean isSpecial, boolean isAfterChristmas) {
+        this.isWeekday = isWeekday;
+        this.isWeekend = isWeekend;
+        this.isSpecial = isSpecial;
+        this.isAfterChristmas = isAfterChristmas;
+    }
+    public boolean isWeekday() {
+        return isWeekday;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public boolean isSpecial() {
+        return isSpecial;
+    }
+
+    public boolean isAfterChristmas() {
+        return isAfterChristmas;
+    }
+}
