bac0dfda3ddb6b5417cc18107247c2f30e634dc2
Lee Geun Woo <gamebdy@naver.com>
2023-11-15 14:07:13 +0900

    :sparkles: checkDate 분리 후 기능을 입력/할인전 기능/할인후 기능 으로 분류

--- src/main/java/christmas/Controller/Controller.java
+++ src/main/java/christmas/Controller/Controller.java
@@ -1,9 +1,12 @@
 package christmas.Controller;
 
+import christmas.Model.EventProperties;
 import christmas.View.InputView;
 import christmas.View.OutputView;
 import christmas.util.CheckCalendar;
 import christmas.Model.Menu;
+import christmas.Model.EventChecker;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,59 +16,49 @@ public class Controller {
 
     private final InputView inputView;
     private final OutputView outputView;
-    private final CheckCalendar checkCalendar;
-    boolean isWeekday;
-    boolean isWeekend;
-    boolean isSpecial;
-    boolean isAfterChristmas;
-    int date = 0;
-    int totalBenefitPrice = 0;
-
-
+    private final EventChecker eventChecker;
     private final Menu menu;
 
-    private Map<String, Integer> menuMap;
-    boolean gift = false;
-    private final List<String> drinkMenuList = Arrays.asList("초코케이크", "아이스크림");
-    private final List<String> weekendMenuList = Arrays.asList("티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타");
+    private final List<String> weekdayDiscountList = Arrays.asList("초코케이크", "아이스크림");
+    private final List<String> weekendDiscountList = Arrays.asList("티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타");
 
 
-    public Controller(InputView inputView, OutputView outputView, CheckCalendar checkCalendar, Menu menu) {
+    public Controller(InputView inputView, OutputView outputView, Menu menu,EventChecker eventChecker) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.checkCalendar = checkCalendar;
         this.menu = menu;
+        this.eventChecker = eventChecker;
     }
 
     public void run() {
-        date = inputView.readDate();
-        checkDate(date);
-        menuMap = inputView.readMenu();
-        printInformation(date);
-        printMenu(menuMap);
+        int date = inputView.readDate();
+        Map<String, Integer> menuMap = inputView.readMenu();
         int totalPrice = calculateTotalPrice(menuMap);
-        printTotalPrice(totalPrice);
-        printGiftOrNot(totalPrice);
 
-        calculateBenefit(totalPrice);
+        beforeApplyBenefit(date, totalPrice, menuMap);
+
+        afterApplyBenefit(date, totalPrice, menuMap);
+    }
+
+    private void beforeApplyBenefit(int date, int totalPrice, Map<String, Integer> menuMap) {
+        outputView.printInformation(date);
+        outputView.printMenu(menuMap);
+        outputView.printTotalPrice(totalPrice);
+    }
+
+    private void afterApplyBenefit(int date, int totalPrice, Map<String, Integer> menuMap) {
+        EventProperties eventProperties = eventChecker.checkDate(date);
+
+        int totalBenefitPrice = calculateBenefit(totalPrice, date, menuMap, eventProperties);
         int expectedPrice = totalPrice - totalBenefitPrice;
 
-        printTotalBenefitPrice(totalBenefitPrice);
-        printPriceAfterDiscount(expectedPrice);
-        printEventBadge(expectedPrice);
+        outputView.printBenefitMenu(totalPrice);
+        outputView.printTotalBenefitPrice(totalBenefitPrice, totalPrice);
+        outputView.printPriceAfterDiscount(expectedPrice);
+        outputView.printBadge(totalBenefitPrice);
+
     }
 
-    private void checkDate(int date) {
-        int dayOfWeek = checkCalendar.getDayOfWeek(date);
-        isWeekday = checkCalendar.isWeekday(dayOfWeek);
-        isWeekend = checkCalendar.isWeekend(dayOfWeek);
-        isSpecial = checkCalendar.isSpecial(date, dayOfWeek);
-        isAfterChristmas = checkCalendar.isAfterChristmas(date);
-    }
-
-    private void printInformation(int date) {
-        outputView.printInformation(date);
-    }
 
     private int calculateTotalPrice(Map<String, Integer> menuMap) {
         int totalPrice = 0;
@@ -73,76 +66,55 @@ public class Controller {
         for (Map.Entry<String, Integer> entry : menuMap.entrySet()) {
             String menuName = entry.getKey();
             int count = entry.getValue();
-            int menuPrice = menu.getPrice(menuName); // 수정된 부분
-
+            int menuPrice = menu.getPrice(menuName);
             totalPrice += menuPrice * count;
         }
 
         return totalPrice;
     }
 
-    private void printMenu(Map<String, Integer> menu) {
-        outputView.printMenu(menu);
-    }
 
-    private void printTotalPrice(int totalPrice) {
-        outputView.printTotalPrice(totalPrice);
-    }
 
-    private int dDayDiscount(int date) {
-        if (isAfterChristmas) return 0;
+    private int dDayDiscount(int date, EventProperties eventProperties) {
+        if (eventProperties.isAfterChristmas()) return 0;
         return 1000 + (date - 1) * 100;
     }
 
-    private int weekDiscount() {
+    private int weekDiscount(Map<String, Integer> menuMap, EventProperties eventProperties) {
         int discount = 0;
         for (Map.Entry<String, Integer> entry : menuMap.entrySet()) {
             String menu = entry.getKey();
             int count = entry.getValue();
 
-            if ((isWeekday && drinkMenuList.contains(menu)) || (isWeekend && weekendMenuList.contains(menu))) {
+            if ((eventProperties.isWeekday() && weekdayDiscountList.contains(menu)) || (eventProperties.isWeekend() && weekendDiscountList.contains(menu))) {
                 discount += count * 2023;
             }
         }
         return discount;
     }
 
-    private int specialDiscount() {
-        if (isSpecial) return 1000;
+    private int specialDiscount(EventProperties eventProperties) {
+        if (eventProperties.isSpecial()) return 1000;
         return 0;
     }
 
-    private void printGiftOrNot(int totalPrice) {
-        if (totalPrice >= 120000) gift = true;
-        outputView.printBenefitMenu(gift);
+    private void printBenefitList(int dDay, int week, int special, int totalPrice) {
+        outputView.printBenefitList(dDay, week, special, totalPrice);
     }
 
-    private void printBenefitList(int dDay, int week, int special) {
-        outputView.printBenefitList(dDay, week, special, gift);
-    }
-
-    private void printTotalBenefitPrice(int totalBenefitPrice) {
-        outputView.printTotalBenefitPrice(totalBenefitPrice, gift);
-    }
-
-    private void printPriceAfterDiscount(int expectedPrice) {
-        outputView.printPriceAfterDiscount(expectedPrice);
-    }
-
-    private void printEventBadge(int totalBenefitPrice) {
-        outputView.printBadge(totalBenefitPrice);
-    }
-
-    private void calculateBenefit(int totalPrice) {
-        if (totalPrice < 10000) printBenefitList(0, 0, 0);
+    private int calculateBenefit(int totalPrice, int date, Map<String, Integer> menuMap, EventProperties eventProperties) {
+        int benefitPrice = 0;
+        if (totalPrice < 10000) printBenefitList(0, 0, 0, 0);
         if (totalPrice >= 10000) {
-            totalBenefitPrice += dDayDiscount(date);
-            totalBenefitPrice += weekDiscount();
-            totalBenefitPrice += specialDiscount();
-            printBenefitList(dDayDiscount(date), weekDiscount(), specialDiscount());
+            int dDay = dDayDiscount(date, eventProperties);
+            int week = weekDiscount(menuMap, eventProperties);
+            int special = specialDiscount(eventProperties);
+            benefitPrice += dDay;
+            benefitPrice += week;
+            benefitPrice += special;
+            printBenefitList(dDay, week, special, totalPrice);
         }
+        return benefitPrice;
     }
-
-
 
 }
